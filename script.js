465a20db6c4b35c2160209ccd98e0308846bb17a
arunkumarbelur <arunkumar.belur@gmail.com>
2026-05-01 17:29:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToCourses() {
+    window.location.href = "courses.html";
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("Thank you! Your message has been sent.");
+}
